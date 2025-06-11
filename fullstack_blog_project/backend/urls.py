ad7e53149920514a618a7bf1aa099d836b1816ef
riadhchelmouni <56774479+riadhchelmouni@users.noreply.github.com>
2025-06-11 16:17:26 +0100

    Update urls.py

--- fullstack_blog_project/backend/urls.py
+++ fullstack_blog_project/backend/urls.py
@@ -10,8 +10,7 @@ urlpatterns = [
     path('api/', include('blog.urls')), # This is where your API lives
 ]
 
-# ONLY add these lines if DEBUG is True. For production, static/media
-# files are served by a web server (like Nginx, Apache, or a CDN).
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # This line might be the culprit.
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
